fd6df5196912146202fc8abb7a354bf2221885fd
Carlos Gil <cgil1210@gmail.com>
2013-08-29 12:03:16 -0400

    word config, gets phrases and synthesizer reads words

--- config-voice.sh
+++ config-voice.sh
@@ -3,10 +3,19 @@
 cd ./sound
 for file in $( ls ); do
 	if [[ $file == *.flac ]]; then
-		echo $file
+		echo "1) Processing file: $file"
 		fileName=`wget -q -U "Mozilla/5.0" --post-file $file --header="Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" |
 		sed 's/.*utterance":"//' | sed 's/","confidence.*//'`
-		echo $fileName
-		mv $file ./final/${fileName}.wav
+		echo "2) Transcriped fileName: $fileName for file: $file"
+		if [[ ! -z "$fileName" ]]; then	#if fileName is set and not empty
+			safeName=${fileName// /_}
+			if [[ $safeName =~ .*_.* ]]; then	
+   				mv $file ./final/phrase/${safeName}.wav	#save the phrase
+   			else 
+   				mv $file ./final/${safeName}.wav	#save the word
+			fi
+		else	#file came back as empty, so remove it
+			rm $file
+		fi
 	fi
 done
--- synthesize-speech.sh
+++ synthesize-speech.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
 
+cd ./text
+for file in $( ls ); do
+	if [[ $file == *.txt ]]; then
+		while IFS='{ }' read word; done	#read file word by word
+		   echo $word
+		done < $file
+	fi
+done
