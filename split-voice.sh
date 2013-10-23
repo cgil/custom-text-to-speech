6e348cbb75dc551597409c95c0e5972980529245
Carlos Gil <cgil1210@gmail.com>
2013-10-23 00:51:48 -0400

    added several sound files

--- split-voice.sh
+++ split-voice.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rec -r 16000 -2 -s -p silence 1 0.1 1% 1 10:00 1% | \
-     sox -p ./sound/voice.flac silence 1 0.1 1% 1 0.1 1% : \
+     sox -p ./sound/voice.flac silence 1 0.1 1% 1 0.05 1% : \
      newfile : restart
 
 #sox voice.flac out.wav silence 1 0.1 1% 1 0.1 1% : newfile : restart
